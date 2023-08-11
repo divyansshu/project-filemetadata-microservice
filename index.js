dc47046cac43bd62cb205c692a162faea562ba0c
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2023-08-11 12:27:31 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
+const multer = require("multer");
+const upload = multer();
+const bodyParser = require("body-parser");
 
 var app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +19,12 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single('upfile'), (req,res,next) => {
 
+  const file = req.file;
+  console.log(file);
+res.json({name:file.originalname, type:file.mimetype, size:file.size});
+});
 
 
 const port = process.env.PORT || 3000;
